1a7e582907a94b162e40b69dc61a82eb5072b572
AVANTGARDE-France <isula@gmx.fr>
2026-09-09 08:37:28 +0200

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,11 @@
+/* =========================================================
+   AVANT-GARDE — SUPABASE
+   js/supabase.js
+
+   Connexion Supabase unique utilisée par les pages du site.
+========================================================= */
+
+window.supabaseClient = window.supabase.createClient(
+  "https://wlhtegrciehmtxqecopv.supabase.co",
+  "sb_publishable_eBPx2UZFdHbLzxSgoBor-Q_FJobVvjs"
+);
