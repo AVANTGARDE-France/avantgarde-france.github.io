2eec99a8fbbfd5856f5b9d69ac0814ce0d67cc85
AVANTGARDE-France <isula@gmx.fr>
2026-09-10 09:33:46 +0200

    Create admin.js

---
+++ js/admin.js
@@ -0,0 +1,23 @@
+/* =========================================================
+   AVANT-GARDE — ADMIN
+   js/admin.js
+
+   Fichier principal de l'administration.
+   Charge les différents modules et lance l'initialisation.
+========================================================= */
+
+import "./admin-auth.js";
+import "./admin-core.js";
+import "./admin-delete-profile.js";
+import "./admin-events.js";
+import "./admin-profile.js";
+import "./admin-role.js";
+import "./admin-tabs.js";
+import "./admin-team.js";
+import "./admin-utils.js";
+
+/* =========================================================
+   INITIALISATION
+========================================================= */
+
+verifierUtilisateur();
